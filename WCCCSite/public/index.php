5d756c1ca7de6178ce822299f547d759c144d015
Tyler Reardon <tdr.reardon@gmail.com>
2017-01-20 10:46:56 -0800

    Initial commit

---
+++ WCCCSite/public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>this is the title</title>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <link href="/resources/css/styles.css" rel="stylesheet">
+  </head>
+  <body>
+        <script src="/resources/js/header.js" type="text/javascript" defer></script>
+    <header></header>
+  </body>
+</html>
---
+++ WCCCSite/public/resources/partials/header.php
@@ -0,0 +1,2 @@
+<script src=<?php echo get_template_directory_uri().'/resources/js/header.js'?> type="text/javascript" defer></script>
+<header></header>
