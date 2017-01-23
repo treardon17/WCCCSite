2def0484ca4fceecc9fe76fc9040626cd31ad872
Tyler Reardon <tdr.reardon@gmail.com>
2017-01-22 18:32:11 -0800

    Improved parallax

--- WCCCSite/src/resources/js/parallax.js
+++ WCCCSite/src/resources/js/parallax.js
@@ -5,8 +5,12 @@ class ParallaxPage{
         let parallaxContainers = $('.parallax-container');
         for(let i = 0; i < parallaxContainers.length; i++){
             let parallaxElements = $(parallaxContainers[i]).find('.parallax-element');
-            this.parallaxContainers.push({container: parallaxContainers[i], elements: parallaxElements})
+            let titleHeadings = $(parallaxContainers[i]).find('.title-heading');
+            this.parallaxContainers.push({container: parallaxContainers[i], elements: parallaxElements, titleHeadings: titleHeadings})
         }
+        //set initial position of elements
+        this.handleScroll();
+        this.currentScrollDirection = 0;
     }
 
     handleScroll(){
@@ -14,13 +18,15 @@ class ParallaxPage{
         for(let i = 0; i < this.parallaxContainers.length; i++){
             let container = this.parallaxContainers[i].container;
             let elements = this.parallaxContainers[i].elements;
-            this.checkElementInView(container, ()=>{
-                //above
-            }, (percentComplete, direction)=>{
-                //middle
-                this.moveElements(elements, -percentComplete*100);
-            }, ()=>{
-                //below
+
+            this.checkElementInView(container,{
+                middleCallback: (percentComplete)=>{
+                    this.moveElements(elements, -percentComplete*250);
+                    this.opacityElements(elements, 1-percentComplete);
+                },
+                bottomProgressCallback: (percentComplete)=>{
+                    
+                }
             })
         }
     }
@@ -31,32 +37,56 @@ class ParallaxPage{
         }
     }
 
+    opacityElements(elements, opacity){
+        for(let i = 0; i < elements.length; i++){
+            $(elements[i]).css({'opacity': opacity});
+        }
+    }
+
     //Checks to see if an element is above, within, or below the current scroll offset
-    checkElementInView(elementSelector, aboveCallback, middleCallback, belowCallback){
+    checkElementInView(elementSelector, cbObject){
         var top  = window.pageYOffset || document.documentElement.scrollTop;
-        var element = $(elementSelector);
-        var height = $(element).outerHeight();
-        var offset = $(element).offset().top;
-        var direction = (this.previousTop < top) ? -1 : 1; //will be -1 if scrolling down, 1 if scrolling up
-        
+        var bottom = top + $(window).height();
+
+        var element = $(elementSelector);       //div being observed
+        var height = $(element).outerHeight();  //height of the element
+        var offset = $(element).offset().top;   //the top of the element
+
+        if(top % 2 == 0){
+            //will be -1 if scrolling down, 1 if scrolling up
+            if(this.previousTop < top){
+                this.currentScrollDirection = -1;
+            }else if(this.previousTop > top){
+                this.currentScrollDirection = 1
+            }
+        }
+
+        this.previousTop = top;
+
         if(top >= offset && top < offset + height){
             //if the top of the viewport is in the middle of the element
-            if(typeof middleCallback === 'function'){
-                var percentComplete = (top - offset)/height;
-                middleCallback(percentComplete, direction);
+            if(typeof cbObject.middleCallback === 'function'){
+                let percentComplete = (top - offset)/height;
+                cbObject.middleCallback(percentComplete);
             }
-        } else if(top - offset < height){
+        }else if(top - offset < height){
             //if the viewport is above the view
-            if(typeof aboveCallback === 'function'){
-                aboveCallback();
+            if(typeof cbObject.aboveCallback === 'function'){
+                cbObject.aboveCallback();
             }
         }else if (top > offset + height){
             //if the viewport is below the view
-            if(typeof belowCallback === 'function'){
-                belowCallback();
+            if(typeof cbObject.belowCallback === 'function'){
+                cbObject.belowCallback();
             }
         }
-        this.previousTop = top;
+
+        if(bottom >= offset && bottom < offset+height){
+            if(typeof cbObject.bottomProgressCallback === 'function'){
+                let percentComplete = (bottom - offset)/height
+                cbObject.bottomProgressCallback(percentComplete);
+            }
+        }
     }
 }
 
