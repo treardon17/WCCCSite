6a858dca0e749a6f0049acd7ab922badd7d86f6e
Tyler Reardon <tdr.reardon@gmail.com>
2017-01-22 21:18:21 -0800

    Added index buttons to image slider

--- WCCCSite/src/components/image-slider/image-slider.js
+++ WCCCSite/src/components/image-slider/image-slider.js
@@ -3,11 +3,15 @@ class ImageSlider{
         this.time = 0; //rotation time
         this.timeout = null; //timer
         this.images = $(imageSliderElement).find('.image-slider-image');
+        this.indices = $(imageSliderElement).find('.image-slider-index');
         if(this.images.length > 0){
             this.currentIndex = 0;
         }
         this.changeImageToIndex(this.currentIndex);
+        this.setupListeners(imageSliderElement);
+    }
 
+    setupListeners(imageSliderElement){
         //Side buttons
         $(imageSliderElement).find('.image-slider-button-right').click((event)=>{
             this.handleRightButton();
@@ -15,16 +19,27 @@ class ImageSlider{
         $(imageSliderElement).find('.image-slider-button-left').click((event)=>{
             this.handleLeftButton();
         });
+
+        //Indices
+        $(imageSliderElement).find('.image-slider-index').click((event)=>{
+            this.handleIndexClicked(event);
+        });
+    }
+
+    handleIndexClicked(event){
+        this.clearClock();
+        let index = $(event.target).attr('imageIndex');
+        this.changeImageToIndex(index);
     }
 
     handleRightButton(){
-        this.nextImage();
         this.clearClock();
+        this.nextImage();
     }
 
     handleLeftButton(){
-        this.prevImage();
         this.clearClock();
+        this.prevImage();
     }
 
     //Clears the clock and restarts the rotation if there is one
@@ -37,32 +52,40 @@ class ImageSlider{
 
     //Change the image to the index specified
     changeImageToIndex(index){
-        if(typeof index != 'undefined' && index < this.images.length && index >= 0){
-            $(this.images).removeClass('image-slider-image-active');
-            $(this.images[index]).addClass('image-slider-image-active');
+        if(typeof index != 'undefined' && index >= 0){
+            if(index < this.images.length){
+                $(this.images).removeClass('image-slider-image-active');
+                $(this.images[index]).addClass('image-slider-image-active');
+            }
+            if(index < this.indices.length){
+                $(this.indices).removeClass('image-slider-index-active');
+                $(this.indices[index]).addClass('image-slider-index-active');
+            }
+            this.currentIndex = index;
         }
     }
 
     //Go to the next image
     nextImage(){
-        if(typeof this.currentIndex === 'undefined')
-            return;
-        if(this.currentIndex + 1 >= this.images.length){
+        if(typeof this.currentIndex === 'undefined') { return; }
+
+        let nextIndex = parseInt(this.currentIndex) + 1
+        if(nextIndex > this.images.length){
             this.currentIndex = 0;
         }else{
-            this.currentIndex += 1;
+            this.currentIndex = nextIndex;
         }
         this.changeImageToIndex(this.currentIndex);
     }
 
     //Go to the previous image
     prevImage(){
-        if(typeof this.currentIndex === 'undefined')
-            return;
-        if(this.currentIndex - 1 < 0){
+        if(typeof this.currentIndex === 'undefined') { return; }
+        let prevIndex = parseInt(this.currentIndex) - 1;
+        if(prevIndex < 0){
             this.currentIndex = this.images.length - 1;
         }else{
-            this.currentIndex -= 1;
+            this.currentIndex = prevIndex;
         }
         this.changeImageToIndex(this.currentIndex);
     }
