6524632b349604065b88ca7827e85c29fe2f0907
Tyler Reardon <tdr.reardon@gmail.com>
2017-01-24 21:44:17 -0800

    Header background changes when not scroll top

--- WCCCSite/src/components/header/header.js
+++ WCCCSite/src/components/header/header.js
@@ -1,10 +1,15 @@
+function handleHeaderScroll(){
+    var top  = window.pageYOffset || document.documentElement.scrollTop;    //top of the window
+    if(top > 0){
+        $('.header-background').addClass('header-background-active');
+    }else{
+        $('.header-background').removeClass('header-background-active');
+    }
+}
+
 $(document).ready(()=>{
+    handleHeaderScroll();
     $(window).scroll(()=>{
-        var top  = window.pageYOffset || document.documentElement.scrollTop;    //top of the window
-        if(top > 0){
-            $('.header-background').addClass('header-background-active');
-        }else{
-            $('.header-background').removeClass('header-background-active');
-        }
+        handleHeaderScroll();   
     });
 });
